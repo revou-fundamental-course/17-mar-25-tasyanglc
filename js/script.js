3ff02c76ece90dcd4295f6dc8dbc41ad87b1383d
tasyanglc <tasyanglch405@gmail.com>
2025-03-24 20:39:03 +0700

    final display

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
 // Javascript Eksternal
+function showSection(sectionId) {
+    document.getElementById('persegi').style.display = 'none';
+    document.getElementById('persegipanjang').style.display = 'none';
+    document.getElementById(sectionId).style.display = 'block';
+  }
+  
